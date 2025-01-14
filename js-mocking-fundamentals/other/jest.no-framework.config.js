587a36b48e8835c185a485e0292f2b08fab85472
belapferreira <isabelapenhaferreira@gmail.com>
2025-01-14 08:28:20 -0300

    chore: update jest config files

--- js-mocking-fundamentals/other/jest.no-framework.config.js
+++ js-mocking-fundamentals/other/jest.no-framework.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   rootDir: path.join(__dirname, '../'),
   roots: [path.join(__dirname, '../src')],
-  displayName: 'jest',
+  displayName: 'no-framework',
   testMatch: ['**/no-framework/**/*.js'],
   runner: require.resolve('./create-node-runner'),
 }
--- js-mocking-fundamentals/src/jest.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  projects: ['./other/jest.config.js', './other/no-framework/jest.config.js'],
-}
