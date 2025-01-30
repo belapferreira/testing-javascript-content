a404cbde58f55520b3c52d80ad9ded25bc0679f8
belapferreira <isabelapenhaferreira@gmail.com>
2025-01-30 09:20:04 -0300

    feat: add jest config with test environment option

---
+++ jest-config/jest.config.js
@@ -0,0 +1,6 @@
+  // Run in a node environment
+  // testEnvironment: 'jest-environment-jsdom',
+
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+}
--- jest-config/src/shared/__tests__/utils.js
+++ jest-config/src/shared/__tests__/utils.js
@@ -3,3 +3,8 @@ import {getFormattedValue} from '../utils'
 test('formats the value', () => {
   expect(getFormattedValue('1234.0')).toBe('1,234.0')
 })
+
+// console.log(window)
+
+// Run in a node environment (without jest config)
+// yarn test --env=node
