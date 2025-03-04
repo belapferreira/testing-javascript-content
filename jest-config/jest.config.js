70807704c4248f559ee15532341fe3d845c7ca80
belapferreira <isabelapenhaferreira@gmail.com>
2025-03-04 11:05:48 -0300

    feat: support importing CSS files

--- jest-config/jest.config.js
+++ jest-config/jest.config.js
@@ -1,6 +1,9 @@
   // Run in a node environment
-  // testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: 'jest-environment-node',
 
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.css$': require.resolve('./test/style-mock.js'),
+  },
 }
