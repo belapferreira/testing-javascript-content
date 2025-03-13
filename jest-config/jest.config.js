e80eeec6becb47f6485a0b90035185b29c8a3458
belapferreira <isabelapenhaferreira@gmail.com>
2025-03-13 08:52:41 -0300

    feat: add a code coverage threshold to maintain code coverage levels

--- jest-config/jest.config.js
+++ jest-config/jest.config.js
@@ -18,4 +18,18 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 34,
+      branches: 19,
+      functions: 30,
+      lines: 29,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    }
+  },
 }
