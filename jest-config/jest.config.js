e89469c8ebd09482833121a37b3d5f1f632a446e
belapferreira <isabelapenhaferreira@gmail.com>
2025-03-13 08:37:28 -0300

    feat: config Jest to report code coverage on project files

--- jest-config/jest.config.js
+++ jest-config/jest.config.js
@@ -17,4 +17,5 @@ module.exports = {
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
+  collectCoverageFrom: ['**/src/**/*.js'],
 }
