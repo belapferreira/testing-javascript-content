e1158cf8444a3d48a99072e4aac28953aecc8865
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-08 11:29:01 -0300

    feat: use Jest DOM for improved assertions

--- jest-and-react-testing-library/src/__tests__/react-dom.js
+++ jest-and-react-testing-library/src/__tests__/react-dom.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {FavoriteNumber} from '../favorite-number'
 
+// This is no needed because the project has a config to import it automatically
+// in every test
+// import '@testing-library/jest-dom/extend-expect'
+
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
 
-  expect(div.querySelector('input').type).toBe('number')
-  // eslint-disable-next-line jest-dom/prefer-to-have-text-content
-  expect(div.querySelector('label').textContent).toBe('Favorite Number')
+  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
