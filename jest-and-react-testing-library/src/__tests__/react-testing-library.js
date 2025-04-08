83660dc0b64ead631bed7bcf3e670bcdc22d67bd
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-08 14:26:49 -0300

    feat: debug the DOM state during the tests

--- jest-and-react-testing-library/src/__tests__/react-testing-library.js
+++ jest-and-react-testing-library/src/__tests__/react-testing-library.js
@@ -7,4 +7,7 @@ test('renders a number input with a label "Favorite Number"', () => {
 
   const input = screen.getByLabelText(/favorite Number/i)
   expect(input).toHaveAttribute('type', 'number')
+
+  // Just to show that is possible to debug things while testing
+  // screen.debug(input)
 })
