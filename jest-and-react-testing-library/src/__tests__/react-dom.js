ee5d48819cea6f195ada2fcbd5b6247e7b65a212
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-08 11:45:35 -0300

    feat: use DOM Testing Library to improve maintainability

--- jest-and-react-testing-library/src/__tests__/react-dom.js
+++ jest-and-react-testing-library/src/__tests__/react-dom.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// eslint-disable-next-line testing-library/no-dom-import
+import {getQueriesForElement} from '@testing-library/dom'
 import {FavoriteNumber} from '../favorite-number'
-
-// This is no needed because the project has a config to import it automatically
-// in every test
-// import '@testing-library/jest-dom/extend-expect'
 
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
 
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  const {getByLabelText} = getQueriesForElement(div)
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const input = getByLabelText(/favorite Number/i)
+  expect(input).toHaveAttribute('type', 'number')
 })
