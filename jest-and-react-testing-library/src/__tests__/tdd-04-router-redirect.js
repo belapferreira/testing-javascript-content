a7f9aae58936909e013d7ed7f115142365eef3a3
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-10 09:10:32 -0300

    feat: TDD mocking react-router's redirect component on a form submission

---
+++ jest-and-react-testing-library/src/__tests__/tdd-04-router-redirect.js
@@ -0,0 +1,53 @@
+/* eslint-disable testing-library/prefer-explicit-assert */
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Redirect as MockRedirect} from 'react-router'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-04-router-redirect'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test('renders a form with title, content, tags, and a submit button', async () => {
+  mockSavePost.mockResolvedValueOnce()
+
+  const fakeUser = {
+    id: 'user-1',
+  }
+
+  const fakePost = {
+    title: 'Test title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+
+  render(<Editor user={fakeUser} />)
+
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags.join(',')
+
+  const submitButton = screen.getByText(/submit/i)
+
+  userEvent.click(submitButton)
+
+  expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  await waitFor(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
+})
