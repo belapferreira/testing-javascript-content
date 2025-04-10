6489be1aa25b80d6c16de090cc8c0e95b2b10a97
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-10 10:01:51 -0300

    feat: add a custom render function to share code between tests and simplify them

---
+++ jest-and-react-testing-library/src/__tests__/tdd-08-custom-render.js
@@ -0,0 +1,90 @@
+/* eslint-disable testing-library/prefer-explicit-assert */
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {build, fake, sequence} from 'test-data-bot'
+import {Redirect as MockRedirect} from 'react-router'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-08-custom-render'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+const postBuilder = build('Post').fields({
+  title: fake(f => f.lorem.words()),
+  content: fake(f => f.lorem.paragraph().replace(/\r/g, '')),
+  tags: fake(f => [f.lorem.word(), f.lorem.word(), f.lorem.word()]),
+})
+
+const userBuilder = build('User').fields({
+  id: sequence(s => `user-${s}`),
+})
+
+function renderEditor() {
+  const fakeUser = userBuilder()
+  const fakePost = postBuilder()
+
+  render(<Editor user={fakeUser} />)
+
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags.join(',')
+
+  const submitButton = screen.getByText(/submit/i)
+
+  return {
+    fakeUser,
+    fakePost,
+    submitButton,
+  }
+}
+
+test('renders a form with title, content, tags, and a submit button', async () => {
+  mockSavePost.mockResolvedValueOnce()
+
+  const {fakePost, fakeUser, submitButton} = renderEditor()
+
+  const preDate = new Date().getTime()
+
+  userEvent.click(submitButton)
+
+  expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    date: expect.any(String),
+    authorId: fakeUser.id,
+  })
+
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  const postDate = new Date().getTime()
+  const date = new Date(mockSavePost.mock.calls[0][0].date).getTime()
+
+  expect(date).toBeGreaterThanOrEqual(preDate)
+  expect(date).toBeLessThanOrEqual(postDate)
+
+  await waitFor(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
+})
+
+test('renders an error message from the server', async () => {
+  const testError = 'test error'
+
+  mockSavePost.mockRejectedValueOnce({data: {error: testError}})
+
+  const {submitButton} = renderEditor()
+
+  userEvent.click(submitButton)
+
+  const postError = await screen.findByRole('alert')
+  expect(postError).toHaveTextContent(testError)
+  expect(submitButton).toBeEnabled()
+})
