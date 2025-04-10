faf3df7ae48ca76d52e578f999eadb8133ca0596
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-10 09:27:38 -0300

    feat: TDD assertions with dates

---
+++ jest-and-react-testing-library/src/__tests__/tdd-05-dates.js
@@ -0,0 +1,62 @@
+/* eslint-disable testing-library/prefer-explicit-assert */
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Redirect as MockRedirect} from 'react-router'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-05-dates'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test('renders a form with title, content, tags, and a submit button', async () => {
+  mockSavePost.mockResolvedValueOnce()
+
+  const fakeUser = {
+    id: 'user-1',
+  }
+
+  const fakePost = {
+    title: 'Test title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+
+  render(<Editor user={fakeUser} />)
+
+  const preDate = new Date().getTime()
+
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags.join(',')
+
+  const submitButton = screen.getByText(/submit/i)
+
+  userEvent.click(submitButton)
+
+  expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    date: expect.any(String),
+    authorId: fakeUser.id,
+  })
+
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  const postDate = new Date().getTime()
+  const date = new Date(mockSavePost.mock.calls[0][0].date).getTime()
+
+  expect(date).toBeGreaterThanOrEqual(preDate)
+  expect(date).toBeLessThanOrEqual(postDate)
+
+  await waitFor(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
+})
