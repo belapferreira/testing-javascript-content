0af1e4bfc51c921c04eb808b366ae1fe65eb3bf6
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-16 10:31:49 -0300

    feat: install Cypress Testing Library

--- jest-config-and-cypress/cypress/e2e/calculator.js
+++ jest-config-and-cypress/cypress/e2e/calculator.js
@@ -2,12 +2,12 @@ describe('anonymous calculator', () => {
   it('can make calculations', () => {
     cy.visit('http://localhost:8080')
 
-    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)').click()
-    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)').click()
-    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(4)').click()
-    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(5)').click()
+    cy.findByText(/^1$/).click()
+    cy.findByText(/^\+$/).click()
+    cy.findByText(/^2$/).click()
+    cy.findByText(/^=$/).click()
 
-    cy.get('[data-testid=total]').should('have.text', '3')
+    cy.findByTestId('total').should('have.text', '3')
 
   })
 })
