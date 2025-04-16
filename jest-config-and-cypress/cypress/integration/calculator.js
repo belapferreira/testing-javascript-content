d0184a2cc5e264b3df3bbbf8bfd31bbc010d88dc
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-16 09:50:26 -0300

    feat: add the first Cypress test

---
+++ jest-config-and-cypress/cypress/integration/calculator.js
@@ -0,0 +1,13 @@
+describe('anonymous calculator', () => {
+  it('can make calculations', () => {
+    cy.visit('http://localhost:8080')
+
+    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)').click()
+    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)').click()
+    cy.get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(4)').click()
+    cy.get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(5)').click()
+
+    cy.get('[data-testid=total]').should('have.text', '3')
+
+  })
+})
