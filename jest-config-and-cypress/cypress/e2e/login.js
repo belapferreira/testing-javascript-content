0cf479f5e5b4b60217fec9257fa2fc5341513989
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-17 08:23:26 -0300

    feat:isolate tests with custom Cypress commands

--- jest-config-and-cypress/cypress/e2e/login.js
+++ jest-config-and-cypress/cypress/e2e/login.js
@@ -1,28 +1,17 @@
-import {buildUser} from '../support/generate'
-
 describe('login', () => {
   it('should login a existing user', () => {
-    const user = buildUser()
+    cy.createUser().then(user => {
+      cy.visit('/')
+      cy.findByText(/login/i).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByText(/submit/i).click()
 
-    // Register the user
-    cy.request({
-      url: 'http://localhost:3000/register',
-      method: 'POST',
-      body: user,
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+      cy.window().its('localStorage.token').should('be.a', 'string')
+
+      cy.findByTestId('username-display')
+      cy.should('have.text', user.username)
     })
-
-    // Test login
-    cy.visit('/')
-    cy.findByText(/login/i).click()
-    cy.findByLabelText(/username/i).type(user.username)
-    cy.findByLabelText(/password/i).type(user.password)
-    cy.findByText(/submit/i).click()
-
-    // Check if the login is successful
-    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
-    cy.window().its('localStorage.token').should('be.a', 'string')
-
-    cy.findByTestId('username-display')
-    cy.should('have.text', user.username)
   })
 })
