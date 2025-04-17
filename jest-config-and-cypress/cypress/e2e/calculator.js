c5123a996538cc2fc9bcae312993bfa4a385da34
belapferreira <isabelapenhaferreira@gmail.com>
2025-04-17 08:45:34 -0300

    feat: combine custom Cypress commands into a single one

--- jest-config-and-cypress/cypress/e2e/calculator.js
+++ jest-config-and-cypress/cypress/e2e/calculator.js
@@ -19,9 +19,7 @@ describe('anonymous calculator', () => {
 
 describe('authenticated calculator', () => {
   it('displays the username', () => {
-    cy.createUser().then(user => {
-      cy.login(user)
-
+    cy.loginAsNewUser().then(user => {
       cy.visit('/')
 
       cy.findByTestId('username-display')
